1c78c61d950fe2fc13f78e7a93134ee017078edb
Tim <tim.rosenbach@icloud.com>
2024-01-30 22:19:04 +0100

    add side menu + Routerlinks

--- restaurant.frontend/src/App.vue
+++ restaurant.frontend/src/App.vue
@@ -1,10 +1,28 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
 </script>
+<script lang="ts">
+function openNav() {
+    window.document.getElementById("mySidenav")!.style.width = "250px";
 
+  }
+  function closeNav() {
+    window.document.getElementById("mySidenav")!.style.width = "0";
+
+  }
+</script>
 <template>
   <div class="app">
+    <span style="font-size:30px;cursor:pointer" @click="openNav()">&#9776</span>
     <router-view />
+  </div>
+  <div id="mySidenav" class="sidenav">
+    <a href="javascript:void(0)" class="closebtn" @click="closeNav()">&times;</a>
+    <router-link to="/">Home</router-link>
+    <router-link to="/menu">Menu</router-link>
+    <router-link to="#">About</router-link>
+    <router-link to="#">Contact</router-link>
+    <router-link to="#">Login</router-link>
   </div>
 </template>
 <style>
@@ -41,5 +59,45 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+/* Menu */
+.sidenav {
+  height: 100%;
+  width: 0;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color: #111;
+  overflow-x: hidden;
+  transition: 0.5s;
+  padding-top: 60px;
+}
+
+.sidenav a {
+  padding: 8px 8px 8px 32px;
+  text-decoration: none;
+  font-size: 25px;
+  color: #818181;
+  display: block;
+  transition: 0.3s;
+}
+
+.sidenav a:hover {
+  color: #f1f1f1;
+}
+
+.sidenav .closebtn {
+  position: absolute;
+  top: 0;
+  right: 25px;
+  font-size: 36px;
+  margin-left: 50px;
+}
+
+@media screen and (max-height: 450px) {
+  .sidenav {padding-top: 15px;}
+  .sidenav a {font-size: 18px;}
 }
 </style>
